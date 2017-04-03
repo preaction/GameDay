be9c9ad21f36624d73475e1570c44c3502c9c4b1
Doug Bell <doug@preaction.me>
2017-04-03 13:47:00 -0500

    add player list component
    This component allows the user to see the player list.
    
    Since Vue components are initialized immediately with their data, and
    since the player list is edited by adding new games, I added a way to
    refresh the tab when the tab changes so that the new data can be seen.
    If the data component implements the "refresh" method, it will be called
    and the data will be refreshed.

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,18 @@
     <div id="app" class="container-fluid">
         <ul class="nav nav-tabs" role="tablist">
             <li role="presentation" class="active">
-                <a href="#games" @click="section = 'Games'" aria-controls="games" role="tab" data-toggle="tab">Games</a>
+                <a href="#games" @click="change_section('Games')" aria-controls="games" role="tab" data-toggle="tab">Games</a>
             </li>
             <li role="presentation">
-                <a href="#players" @click="section = 'Players'" aria-controls="players" role="tab" data-toggle="tab">Players</a>
+                <a href="#players" @click="change_section('Players')" aria-controls="players" role="tab" data-toggle="tab">Players</a>
             </li>
         </ul>
         <div class="tab-content">
             <div role="tabpanel" class="tab-pane active" id="games">
                 <game-list />
             </div>
-            <div role="tabpanel" class="tab-pane active" id="players">
-                <!--<player-list />-->
+            <div role="tabpanel" class="tab-pane" id="players">
+                <player-list ref="Players" />
             </div>
         </div>
     </div>
@@ -41,10 +41,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap-theme.css";
 import "bootstrap";
 import GameList from "./components/game-list.vue";
+import PlayerList from "./components/player-list.vue";
 
 export default {
     name: 'app',
-    components: { GameList },
+    components: { GameList, PlayerList },
     data() {
         return {
             section: "Welcome",
@@ -52,6 +53,14 @@ export default {
     },
     computed: {
         title() { return this.section + " - GameDay"; },
+    },
+    methods: {
+        change_section( section ) {
+            this.section = section;
+            if ( this.$refs[section] && this.$refs[section].refresh ) {
+                this.$refs[section].refresh();
+            }
+        },
     },
     watch: {
         title() {
---
+++ src/components/player-list.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div id="player-list">
+        <div class="list-group">
+            <div v-for="( player, $index ) in players" class="list-group-item">
+                <h4 class="list-group-item-heading">{{ player.name }}</h4>
+                <p>DCI: {{ player.dci }}</p>
+                <p>Last seen: <time>{{ player.seen }}</time></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Player from "../player.js";
+export default {
+    name: 'player-list',
+    data() {
+        var data = {
+            players: [ ],
+        };
+        return data;
+    },
+    methods: {
+        refresh() {
+            Player.read_all().then(
+                (players) => this.players = players
+            );
+        }
+    }
+};
+</script>
